5ef847a6ea7477be52a3d937de791f28bd4ece81
Goran Latisko <glatisko@gmail.com>
2022-05-31 21:39:04 -0500

    Revert "Delete BookstoreSpringJMS directory"
    This reverts commit 1f7e5a31e092726c6e136c35a7c329c331021752.

---
+++ BookstoreSpringJMS/src/main/java/com/glatisko/SpringJmsExample/service/jms/BookOrderService.java
@@ -0,0 +1,35 @@
+package com.glatisko.SpringJmsExample.service.jms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessagePostProcessor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.glatisko.SpringJmsExample.pojos.BookOrder;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Component
+public class BookOrderService {
+
+    private static final String BOOK_QUEUE = "book.order.queue";
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Transactional
+    public void send(BookOrder bookOrder, String storeId, String orderState){
+        jmsTemplate.convertAndSend(BOOK_QUEUE, bookOrder, new MessagePostProcessor() {
+            @Override
+            public Message postProcessMessage(Message message) throws JMSException {
+                message.setStringProperty("bookOrderId", bookOrder.getBookOrderId());
+                message.setStringProperty("storeId", storeId);
+                message.setStringProperty("orderState", orderState);
+                return message;
+            }
+        });
+    }
+}
